2ba64584e5a3eef996d6724a553ad610c1a060b3
David Simmons <dr.desimmons@gmail.com>
2023-05-10 18:26:16 +0100

    Add template asset

---
+++ README.md
@@ -0,0 +1,13 @@
+# <Insert_your_asset_name>
+
+A description of my asset.
+
+## Prepare input
+
+1. Provide a description of how inputs for my asset can be prepared.
+2. Include any sample code that aids the creation of input data.
+
+## Process output
+
+1. Provide a description of how results fromm my asset are structured.
+2. Include any sample code that aids the consumption of the results.
---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, File
+
+# <ADD DEPENDENCIES>
+
+app = FastAPI()
+
+@app.put("/run/")
+async def infer(input: bytes = File()):
+
+    # You must extract the input to the model from `input`.
+    #
+    # If `input` is a text string:
+    #   text=input.decode("utf-8")
+    #
+    # If `input` is a json object:
+    #   json_input = json.loads(input.decode("utf-8"))
+    #
+    # If `input` is an image:
+    #   image = Image.open(io.BytesIO(input))
+    #
+    # If `input` is an image base64 embedded into a json:
+    #   json_input = json.loads(input.decode("utf-8"))
+    #   img_data = json_input["image"].encode()
+    #   content = base64.b64decode(img_data)
+
+
+    # <PARSE `input`>
+    # <PROCESS `input`>
+    # <RETURN RESULT OF `input` PROCESS>
+
+
+    return {"results": "CALCULATED RESULT"}
